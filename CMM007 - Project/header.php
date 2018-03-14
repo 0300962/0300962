ebcc6baac1f065b7f46679df53e5dc8441c788dc
0300962 <YokoGawa9>
2018-03-14 17:19:17 +0000

    First commit

---
+++ CMM007 - Project/header.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 0300962
+ * Date: 14/03/2018
+ * Time: 17:08
+ */?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+<body>
+    <div class="flex-container">
+        <div id="Login">
+            <?php #Need to include the Login function as a file here ?>
+        </div>
+
+        <div id="logo">
+            <a href="index.php">
+                <img id="logo" src="img\logo.png" alt="logo">
+            </a>
+        </div>
+
+        <div id="navbar">
+            <div>About</div>
+            <div>Projects</div>
+            <div>My Profile</div>
+        </div>
+
+
+    </div>
+
+
+
+
+
+</body>
