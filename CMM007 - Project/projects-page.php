1617211ede3cd3973b46599185c7c92f217e102b
0300962 <YokoGawa9>
2018-03-21 15:59:16 +0000

    21/03/18

--- CMM007 - Project/projects-page.php
+++ CMM007 - Project/projects-page.php
@@ -15,14 +15,31 @@ session_start();
 <body>
     <div class = "container">
         <div id="controls">
-
+            <form name="controls" method="post">
+                <input type="text" name="search" placeholder="Search"><br/>
+                <!-- Tag selector?-->
+                <!-- Deadline selector?-->
+                <input id="go" type="submit" value="Search!">
+            </form>
 
         </div>
         <div id="projectlist">
-
-
-
-
+            <?php
+                /*include_once 'Scripts/connection.php';
+                $sql = "SELECT name, tags, summary, deadline, image
+                        FROM projects
+                        WHERE status = 1";
+                $result = mysqli_query($dbcon, $sql);
+                echo "<table><tr><th></th><th>Project</th><th>Deadline</th></tr>";
+                if(mysqli_num_rows($result) > 0) {
+                    while($row = mysqli_fetch_array($result)){
+                        echo "<tr><td><img src = {$row['image']} alt = 'Project image'></td><td>{$row['name']}</td><td>{$row['summary']}</td></tr>";
+                    };
+                } else {*/
+                    echo "<tr><td colspan='3'>No projects found!</td></tr>";
+             //   }
+                echo "</table>";
+            ?>
         </div>
 
 
--- CMM007 - Project/register-page.php
+++ CMM007 - Project/register-page.php
@@ -53,9 +53,9 @@ session_start();
             <ul>
                 <li>Do not give out any personal information, in profiles or messages.</li>
                 <li>This site is not liable for any data loss that may occur as a result of using it.</li>
+                <li>Login details are stored with a robust hash algorithm, however it is strongly recommended to not reuse passwords on other services.</li>
                 <li>This site may use cookies as part of the login system.</li>
             </ul>
-
         </div>
 
         <div id="form">
