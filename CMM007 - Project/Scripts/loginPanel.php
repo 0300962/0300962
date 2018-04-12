304314788ef0ae5893eb01a9b1b08035173a1fa8
0300962 <0300962@rgu.ac.uk>
2018-04-12 18:52:59 +0100

    12-04-18 - Corrected login fonts

--- CMM007 - Project/Scripts/loginPanel.php
+++ CMM007 - Project/Scripts/loginPanel.php
@@ -12,9 +12,8 @@ if (session_status() === PHP_SESSION_NONE) {
 
 /*Presents login boxes if user is not logged-in*/
 if ((isset($_SESSION['logged-in'])) && ($_SESSION['logged-in'] == TRUE)) {
-        echo "Welcome, {$_SESSION['name']}!";
-        echo "<div id='login'>
-            <form name='form' action='Scripts/login.php' method='post'><br/>
+        echo "<div id='login'>Welcome, {$_SESSION['name']}!";
+        echo "<form name='form' action='Scripts/login.php' method='post'><br/>
             <input id='logout' type='submit' name='logout' value='Logout'><br/>
             </form><br/>
             <a href='messages.php' type='button'>Messages</a>
@@ -25,7 +24,7 @@ if ((isset($_SESSION['logged-in'])) && ($_SESSION['logged-in'] == TRUE)) {
             Welcome! Please sign in:<br/>
             <input type='text' name='username' placeholder='Email Address' ><br/>
             <input type='password' name='password' placeholder='Password'><br/>
-            <input id='login' type='submit' name='login' value='Sign in'>
+            <input type='submit' name='login' value='Sign in'>
         </form><br/>
         Or Register: <a href='register.php' type='button'>Register</a>
     </div>"; /*Link to register.php shows as wrong, but is fine once embedded on page */
